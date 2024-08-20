013182ed8a1f78697b83a002dc38d23e59b48d5a
Feruza Orifjonova <feruza.orifjonova1997@gmail.com>
2024-08-20 00:45:14 +0000

    Allow download image using function from data_management

--- app_pages/mildew_detector_page.py
+++ app_pages/mildew_detector_page.py
@@ -29,3 +29,27 @@ def mildew_detector_body():
     # The file uploader is shown with three extensions allowed, png, jpg and jpeg.
     images_buffer = st.file_uploader('Upload leaves images samples. You may select more than one.',
                                         type=['png','jpg','jpeg'],accept_multiple_files=True)
+
+    # if the user load at least one image, we can analyze the image in real time and show them 
+    # the report and allow download it using the function imported from data_managment
+    if images_buffer is not None:
+        df_report = pd.DataFrame([])
+        for image in images_buffer:
+
+            img_pil = (Image.open(image))
+            st.info(f"Leaf image Sample: **{image.name}**")
+            img_array = np.array(img_pil)
+            st.image(img_pil)
+
+            version = 'v1'
+            resized_img = resize_input_image(img=img_pil, version=version)
+            pred_proba, pred_class = load_model_and_predict(resized_img, version=version)
+            plot_predictions_probabilities(pred_proba, pred_class)
+
+            df_report = df_report.append({"Name":image.name, 'Result': pred_class },
+                                        ignore_index=True)
+        
+        if not df_report.empty:
+            st.success("Analysis Report")
+            st.table(df_report)
+            st.markdown(download_dataframe_as_csv(df_report), unsafe_allow_html=True)
