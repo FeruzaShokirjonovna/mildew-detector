ffe79be091d56e7979c3b37039543b88ba3635f9
Feruza Orifjonova <feruza.orifjonova1997@gmail.com>
2024-08-20 01:15:02 +0000

    Add code toshow the warning message

--- app_pages/summary_page.py
+++ app_pages/summary_page.py
@@ -9,10 +9,23 @@ def check_warning_message_state():
 
     if 'warning_shown' not in st.session_state:
         st.session_state.warning_shown = False
+        
 
 # Function to show the warning message
 def show_warning_message():
-    
+    '''
+    Displays the warning message recommending light mode over dark mode.
+    '''
+
+    if not st.session_state.warning_shown:
+        placeholder = st.empty()
+        placeholder.markdown('<div style="background-color: #FFEEEB; padding: 30px; margin-top: 40px; border-radius: 5px; text-align: center;"><p style="font-size: 20px; color: #333333"><strong>For better visualization, it is recommended to use Light mode instead of Dark mode in Settings.</strong></p></div>', unsafe_allow_html=True)
+        st.session_state.warning_shown = True
+
+        time.sleep(5)  # Wait for 5 seconds
+        placeholder.empty()
+
+
 def summary_page_body():
     """ Funtion to show the summary page """
     st.write("### Quick Project Summary")
