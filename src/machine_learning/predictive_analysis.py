42caef7d4f8f1b0d1a8227b91ca923d7200173e8
Feruza Orifjonova <feruza.orifjonova1997@gmail.com>
2024-08-20 00:25:55 +0000

    Resize input image

--- src/machine_learning/predictive_analysis.py
+++ src/machine_learning/predictive_analysis.py
@@ -39,6 +39,17 @@ def plot_predictions_probabilities(pred_proba, pred_class):
             )
     st.plotly_chart(fig)
 
+
+def resize_input_image(img, version):  
+    """
+    Reshape image to average image size
+    """
+    image_shape = load_pkl_file(file_path=f"outputs/{version}/image_shape.pkl")
+    img_resized = img.resize((image_shape[1], image_shape[0]), Image.ANTIALIAS)
+    my_image = np.expand_dims(img_resized, axis=0)/255
+
+    return my_image
+
 def load_model_and_predict(my_image, version):
     """
     Load and perform ML prediction over live images
